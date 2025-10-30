a1a2a3b1526531775afc2530815e7b1e40e39add
yashtapre77 <yashtapre77@gmail.com>
2025-10-30 10:15:20 +0530

    exam platform frontend

--- exam_platform/backend/main.py
+++ exam_platform/backend/main.py
@@ -2,8 +2,15 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from pinecone_utils import get_all_context_from_pinecone
 from langchain_pipeline import generate_mcqs_from_context
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(title="Exam Platform ")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*']
+)
 
 class TopicRequest(BaseModel):
     topic: str
